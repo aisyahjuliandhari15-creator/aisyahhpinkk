d21e2686ed2f861a44f19ece712b9f00b4a6f647
Muhammad Syamsul Ma'rif <muhdevapp@gmail.com>
2026-07-14 10:06:24 +0800

    update perubahan <samsul>

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-console.log('hello jsloe')
-
-var msg = "saya baru belajar bahasa indonsia"
+var msg = "saya belajar bahasa indo"
 
 console.log(msg)
 var x = 1
-var y = 2
+var y = 4
 
 if(x + y == 3) {
     console.log('yee 3 jir')
