0d5bfa7ae8abed42af30b014e449fdbec1f72e9c
aisyahjuliandhari15 <aisyahjuliandhari15@gmail.com>
2026-07-14 10:17:21 +0700

    saya mengubah var msg dan var y

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 console.log('hello jsloe')
 
-var msg = "saya baru belajar bahasa indonsia"
+var msg = "saya baru belajar bahasa prancis"
 
 console.log(msg)
 var x = 1
-var y = 2
+var y = 3
 
 if(x + y == 3) {
     console.log('yee 3 jir')
